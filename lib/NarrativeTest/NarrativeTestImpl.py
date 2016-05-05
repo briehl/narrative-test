b70d59c374ffc81b7f46e4459a110c61452ad2c6
Bill Riehl <wjriehl@lbl.gov>
2016-05-05 15:59:11 -0700

    Updated the simple async copy method.

--- lib/NarrativeTest/NarrativeTestImpl.py
+++ lib/NarrativeTest/NarrativeTestImpl.py
@@ -26,7 +26,7 @@ class NarrativeTest:
     VERSION = "0.0.1"
     GIT_URL = "https://github.com/briehl/narrative-test"
     GIT_COMMIT_HASH = "407b5e2a5b4d25f470466d466b2eaef894a246c7"
-    
+
     #BEGIN_CLASS_HEADER
     # Class variables and functions can be defined in this block
     workspaceURL = None
@@ -39,16 +39,16 @@ class NarrativeTest:
         self.workspaceURL = config['workspace-url']
         #END_CONSTRUCTOR
         pass
-    
+
 
     def filter_contigs(self, ctx, params):
         # ctx is the context object
         # return variables are: returnVal
         #BEGIN filter_contigs
-        
+
         # Print statements to stdout/stderr are captured and available as the method log
         print('Starting filter contigs method.')
-        
+
 
         # Step 1 - Parse/examine the parameters and catch any errors
         # It is important to check that parameters exist and are defined, and that nice error
@@ -69,15 +69,15 @@ class NarrativeTest:
             raise ValueError('Cannot parse integer from min_length parameter (' + str(min_length_orig) + ')')
         if min_length < 0:
             raise ValueError('min_length parameter shouldn\'t be negative (' + str(min_length) + ')')
-        
+
 
         # Step 2- Download the input data
         # Most data will be based to your method by its workspace name.  Use the workspace to pull that data
         # (or in many cases, subsets of that data).  The user token is used to authenticate with the KBase
         # data stores and other services.  DO NOT PRINT OUT OR OTHERWISE SAVE USER TOKENS
         token = ctx['token']
-        wsClient = workspaceService(self.workspaceURL, token=token) 
-        try: 
+        wsClient = workspaceService(self.workspaceURL, token=token)
+        try:
             # Note that results from the workspace are returned in a list, and the actual data is saved
             # in the 'data' key.  So to get the ContigSet data, we get the first element of the list, and
             # look at the 'data' field.
@@ -87,9 +87,9 @@ class NarrativeTest:
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
             orig_error = ''.join('    ' + line for line in lines)
             raise ValueError('Error loading original ContigSet object from workspace:\n' + orig_error)
-        
+
         print('Got ContigSet data.')
-        
+
 
         # Step 3- Actually perform the filter operation, saving the good contigs to a new list
         good_contigs = []
@@ -103,9 +103,9 @@ class NarrativeTest:
 
         # replace the contigs in the contigSet object in local memory
         contigSet['contigs'] = good_contigs
-        
+
         print('Filtered ContigSet to '+str(n_remaining)+' contigs out of '+str(n_total))
-        
+
 
         # Step 4- Save the new ContigSet back to the Workspace
         # When objects are saved, it is important to always set the Provenance of that object.  The basic
@@ -135,7 +135,7 @@ class NarrativeTest:
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
             orig_error = ''.join('    ' + line for line in lines)
             raise ValueError('Error saving filtered ContigSet object to workspace:\n' + orig_error)
-        
+
         info = obj_info_list[0]
         # Workspace Object Info is a tuple defined as-
         # absolute ref = info[6] + '/' + info[0] + '/' + info[4]
@@ -163,7 +163,7 @@ class NarrativeTest:
 
         reportObj = {
             'objects_created':[{
-                    'ref':str(info[6]) + '/'+str(info[0])+'/'+str(info[4]), 
+                    'ref':str(info[6]) + '/'+str(info[0])+'/'+str(info[4]),
                     'description':'Filtered Contigs'
                 }],
             'text_message':report
@@ -191,7 +191,7 @@ class NarrativeTest:
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
             orig_error = ''.join('    ' + line for line in lines)
             raise ValueError('Error saving filtered ContigSet object to workspace:\n' + orig_error)
-        
+
         report_info = report_info_list[0]
 
         print('saved Report: '+pformat(report_info))
@@ -204,9 +204,9 @@ class NarrativeTest:
                 'n_contigs_removed':n_total-n_remaining,
                 'n_contigs_remaining':n_remaining
             }
-        
+
         print('returning:'+pformat(returnVal))
-                
+
         #END filter_contigs
 
         # At some point might do deeper type checking...
@@ -220,6 +220,90 @@ class NarrativeTest:
         # ctx is the context object
         # return variables are: returnVal
         #BEGIN test_async_job
+        print("Starting test_async_job")
+        if 'workspace' not in params:
+            raise ValueError('Parameter workspace is not set in input arguments')
+        workspace_name = params['workspace']
+        if 'input_genome_name' not in params:
+            raise ValueError('Parameter input_genome_name is not set in input arguments')
+        input_genome_name = params['input_genome_name']
+        if 'output_genome_name' not in params:
+            raise ValueError('Parameter output_genome_name is not set in input arguments')
+        output_genome_name = params['output_genome_name']
+
+
+        token = ctx['token']
+        wsClient = workspaceService(self.workspaceURL, token=token)
+        print('Attempting to copy genome named "{}" to new name "{}"'.format(input_genome_name, output_genome_name))
+
+        try:
+            info = wsClient.copy_object({
+                       'from': {'ref': workspace_name + '/' + input_genome_id},
+                       'to': {'ref': workspace_name + '/' + output_genome_id}
+                   })
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error copying Genome object in workspace:\n' + orig_error)
+
+        print('Successfully copied genome!')
+        print('Generating report...')
+
+        # Step 5- Create the Report for this method, and return the results
+        # Create a Report of the method
+        report = "New Genome saved to {}/{}/{}\n".format(str(info[7]), str(info[1]), str(info[4]))
+        report += "Successfully copied!\n"
+
+        reportObj = {
+            'objects_created':[{
+                    'ref': str(info[6])+'/'+str(info[0])+'/'+str(info[4]),
+                    'description':'Copied Genome'
+                }],
+            'text_message':report
+        }
+
+        # generate a unique name for the Method report
+        reportName = 'test_async_job_report_'+str(hex(uuid.getnode()))
+        report_info_list = None
+        try:
+            provenance = [{}]
+            if 'provenance' in ctx:
+                provenance = ctx['provenance']
+
+            provenance[0]['input_ws_objects']=[workspace_name+'/'+input_genome_id]
+            report_info_list = wsClient.save_objects({
+                    'id':info[6],
+                    'objects':[
+                        {
+                            'type':'KBaseReport.Report',
+                            'data':reportObj,
+                            'name':reportName,
+                            'meta':{},
+                            'hidden':1, # important!  make sure the report is hidden
+                            'provenance':provenance
+                        }
+                    ]
+                })
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error saving report object to workspace:\n' + orig_error)
+
+        report_info = report_info_list[0]
+
+        print('saved Report: '+pformat(report_info))
+
+        returnVal = {
+                'report_name': reportName,
+                'report_ref': str(report_info[6]) + '/' + str(report_info[0]) + '/' + str(report_info[4]),
+                'new_contigset_ref': str(info[6]) + '/'+str(info[0])+'/'+str(info[4]),
+                'n_initial_contigs':n_total,
+                'n_contigs_removed':n_total-n_remaining,
+                'n_contigs_remaining':n_remaining
+            }
+
         #END test_async_job
 
         # At some point might do deeper type checking...
@@ -231,7 +315,7 @@ class NarrativeTest:
 
     def status(self, ctx):
         #BEGIN_STATUS
-        returnVal = {'state': "OK", 'message': "", 'version': self.VERSION, 
+        returnVal = {'state': "OK", 'message': "", 'version': self.VERSION,
                      'git_url': self.GIT_URL, 'git_commit_hash': self.GIT_COMMIT_HASH}
         #END_STATUS
         return [returnVal]
